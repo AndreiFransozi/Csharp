594c9e1ab05268a28d35ce2c4ba9a50ddea935ad
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2023-07-19 13:40:18 -0300

    Create convert.cs

---
+++ convert.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("Digite qual número é sua casa: ");
+string numeroCasa - Console.ReadLine();
+int numero - Int.Parse(numeroCasa);
+Console.Writeline("Número é sua casa: {0}", numero};
+
+numero = Convert.ToInt32(numeroCasa);
+Console.WriteLine(numero); 

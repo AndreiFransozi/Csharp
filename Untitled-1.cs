400186d62d51a8701797de46c4d05943b7a65f58
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2023-07-19 13:08:59 -0300

    hello world

---
+++ Untitled-1.cs
@@ -0,0 +1,12 @@
+namespace OlaMundo
+{
+    class Comentarios
+    {
+    static void comenta()
+    {
+        /* Console.Write("curso Basico de c");
+        Console.Write("programa para definicao de comentarios");*/
+        Console.Write("Detalhe seu codigo sempre!");
+        }    
+    }
+}

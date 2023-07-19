83114800c5f9db79b32fc94a832fa645c1982ea9
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2023-07-19 13:26:13 -0300

    Add files via upload

---
+++ Ola mundo.cs
@@ -0,0 +1,14 @@
+name space Ola mundo
+{
+    class Variaveis
+    {
+        static void Main(){
+            int x = 4;
+            double y = 3.3;
+            const double frequencia = 60;
+
+            double area = x * y;
+            Console.WriteLine("A área do quadrado é: " + area);
+        }
+    }
+}

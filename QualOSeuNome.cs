12dacd1d3a18d73713af8bd345e41e44e6ac94d7
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2023-07-20 13:34:07 -0300

    Create QualOSeuNome.cs

---
+++ QualOSeuNome.cs
@@ -0,0 +1,8 @@
+Static void Main()
+{
+  Console.WriteLine("Qual seu nome?");
+  String nome = Console.ReadLine();
+  Console.WriteLine("Quantos anos você tem?");
+  int idade - int.Parse(Console.ReadLine());
+  Console.WriteLine("Seu nome é {0} e sua idade é de {1}", nome, idade);
+}

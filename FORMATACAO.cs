abd70c4cf7a1db362f6230f4c0e843efb0f5ed60
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2023-07-20 13:27:50 -0300

    Create FORMATACAO.cs

---
+++ FORMATACAO.cs
@@ -0,0 +1,10 @@
+class Formatação
+{
+static void Main()
+{
+  double x = 20.232;
+  Console.WriteLine(x.ToString("F1"));
+  Console.WriteLine(x.ToString("C"));
+  Console.WriteLine(x.ToString("P"));
+}
+}

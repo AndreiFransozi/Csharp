7b5415548b30c153953720351a854fda79831074
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2023-07-20 14:14:21 -0300

    Create ifelse.cs

---
+++ ifelse.cs
@@ -0,0 +1,15 @@
+var n1
+var n2
+var n3
+
+if (n1 < n2){
+Console.WriteLine("Condição 1 é satisfeita !!");
+Console.WriteLine("Resultado da soma é {0}", n1 + n2);
+}
+if (n3 < n2) {
+ Console.WriteLine(n3 + n2); 
+}
+else
+{
+Console.WriteLine("Condição 2 não é satisfeita!!.");
+}
